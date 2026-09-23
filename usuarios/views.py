84e28ed2e73a37450cd6fbfcb6c696e1b2d853ea
Rafael S S <pegasosrafael@gmail.com>
2026-09-23 08:27:45 -0400

    Correção das urls

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,9 +4,8 @@ from .models import Usuario
 
 def lista_usuarios(request):
     usuarios = Usuario.objects.all()
-
     contexto = {
         'usuarios': usuarios
     }
-
     return render(request, 'usuarios/lista.html', contexto)
+
